1d3910cfcc5c38e4c628e30f6762fa90491a22ea
Adithya2310 <Adithyanandi099@gmail.com>
2023-10-29 12:35:20 +0530

    resolved the corse issue

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig =  {
+    async headers() {
+      return [
+        {
+          source: '/api/:path*', // Matches all API routes
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*', // Allow requests from all origins. Change to specific origin if needed.
+            },
+            // You can add more headers if needed
+            // {
+            //   key: 'Access-Control-Allow-Methods',
+            //   value: 'GET, POST, PUT, DELETE',
+            // },
+          ],
+        },
+      ];
+    },
+  };
 
 module.exports = nextConfig
